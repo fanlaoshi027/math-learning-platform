b8272a3151f202788ce1b92f215a15f1af801805
fanlaoshi027 <fanzeping@gmail.com>
2026-08-28 16:26:45 +0800

    Add Tutor LMS course-system menu and clean dashboard routing

--- wp/wp-content/plugins/math-course/includes/Admin/class-menu.php
+++ wp/wp-content/plugins/math-course/includes/Admin/class-menu.php
@@ -4,144 +4,81 @@ namespace MathCourse\Admin;
 
 defined('ABSPATH') || exit;
 
-
 class Menu
 {
-
-
     public function __construct()
     {
-
-        add_action(
-            'admin_menu',
-            array(
-                $this,
-                'register_menu'
-            )
-        );
-
+        add_action('admin_menu', array($this, 'register_menu'));
     }
-
-
 
     public function register_menu()
     {
-
-
         add_menu_page(
-
             'MathCourse',
-
             'MathCourse',
-
             'manage_options',
-
             'mathcourse',
-
-            array(
-                $this,
-                'dashboard'
-            ),
-
+            array($this, 'dashboard'),
             'dashicons-welcome-learn-more',
-
             30
-
         );
 
-
+        add_submenu_page(
+            'mathcourse',
+            '课程系统',
+            '课程系统',
+            'manage_options',
+            'mathcourse-tutor',
+            array($this, 'tutor_page')
+        );
 
         add_submenu_page(
-
             'mathcourse',
-
             '课程授权',
-
             '课程授权',
-
             'manage_options',
-
             'mathcourse-access',
-
-            array(
-                $this,
-                'access_page'
-            )
-
+            array($this, 'access_page')
         );
-
-
 
         add_submenu_page(
-
             'mathcourse',
-
             '设置',
-
             '设置',
-
             'manage_options',
-
             'mathcourse-settings',
-
-            array(
-                $this,
-                'settings_page'
-            )
-
+            array($this, 'settings_page')
         );
-
-
     }
-
-
 
     public function dashboard()
     {
+        if (class_exists('MathCourse\\Admin\\Dashboard')) {
+            (new Dashboard())->render();
+            return;
+        }
 
-        echo '<div class="wrap">';
-
-        echo '<h1>MathCourse</h1>';
-
-        echo '<p>数学课程管理系统</p>';
-
-        echo '</div>';
-
+        echo '<div class="wrap"><h1>MathCourse</h1><p>数学课程管理系统</p></div>';
     }
 
-
+    public function tutor_page()
+    {
+        if (class_exists('MathCourse\\Admin\\Tutor_Page')) {
+            (new Tutor_Page())->render();
+        }
+    }
 
     public function access_page()
     {
-
-        if(
-            class_exists(
-                'MathCourse\Admin\Access_Page'
-            )
-        ){
-
+        if (class_exists('MathCourse\\Admin\\Access_Page')) {
             (new Access_Page())->render();
-
         }
-
     }
-
-
 
     public function settings_page()
     {
-
-        if(
-            class_exists(
-                'MathCourse\Admin\Settings'
-            )
-        ){
-
+        if (class_exists('MathCourse\\Admin\\Settings')) {
             (new Settings())->render();
-
         }
-
     }
-
-
 }
