582c134176bc7a160d148bef51358c7259cfbcb6
fanlaoshi027 <fanzeping@gmail.com>
2026-08-28 16:28:09 +0800

    Add MathCourse course management menu

--- wp/wp-content/plugins/math-course/includes/Admin/class-menu.php
+++ wp/wp-content/plugins/math-course/includes/Admin/class-menu.php
@@ -13,42 +13,11 @@ class Menu
 
     public function register_menu()
     {
-        add_menu_page(
-            'MathCourse',
-            'MathCourse',
-            'manage_options',
-            'mathcourse',
-            array($this, 'dashboard'),
-            'dashicons-welcome-learn-more',
-            30
-        );
-
-        add_submenu_page(
-            'mathcourse',
-            '课程系统',
-            '课程系统',
-            'manage_options',
-            'mathcourse-tutor',
-            array($this, 'tutor_page')
-        );
-
-        add_submenu_page(
-            'mathcourse',
-            '课程授权',
-            '课程授权',
-            'manage_options',
-            'mathcourse-access',
-            array($this, 'access_page')
-        );
-
-        add_submenu_page(
-            'mathcourse',
-            '设置',
-            '设置',
-            'manage_options',
-            'mathcourse-settings',
-            array($this, 'settings_page')
-        );
+        add_menu_page('MathCourse', 'MathCourse', 'manage_options', 'mathcourse', array($this, 'dashboard'), 'dashicons-welcome-learn-more', 30);
+        add_submenu_page('mathcourse', '课程管理', '课程管理', 'manage_options', 'mathcourse-courses', array($this, 'course_page'));
+        add_submenu_page('mathcourse', '课程系统', '课程系统', 'manage_options', 'mathcourse-tutor', array($this, 'tutor_page'));
+        add_submenu_page('mathcourse', '课程授权', '课程授权', 'manage_options', 'mathcourse-access', array($this, 'access_page'));
+        add_submenu_page('mathcourse', '设置', '设置', 'manage_options', 'mathcourse-settings', array($this, 'settings_page'));
     }
 
     public function dashboard()
@@ -57,8 +26,14 @@ class Menu
             (new Dashboard())->render();
             return;
         }
-
         echo '<div class="wrap"><h1>MathCourse</h1><p>数学课程管理系统</p></div>';
+    }
+
+    public function course_page()
+    {
+        if (class_exists('MathCourse\\Admin\\Course_Page')) {
+            (new Course_Page())->render();
+        }
     }
 
     public function tutor_page()
