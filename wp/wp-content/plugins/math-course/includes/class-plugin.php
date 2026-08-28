e8875afc89655b8f979028e1d92cb1ae5e14b577
fanlaoshi027 <fanzeping@gmail.com>
2026-08-29 00:23:09 +0800

    fix(video): wire HLS storage into protected endpoints

--- wp/wp-content/plugins/math-course/includes/class-plugin.php
+++ wp/wp-content/plugins/math-course/includes/class-plugin.php
@@ -18,24 +18,21 @@ class Plugin
             new Admin\Menu();
         }
 
-        $access_service = null;
-        if (class_exists('MathCourse\\Access\\Access_Service')) {
-            $access_service = new Access\Access_Service();
-        }
+        $access_service = class_exists('MathCourse\\Access\\Access_Service')
+            ? new Access\Access_Service()
+            : null;
 
         if (class_exists('MathCourse\\Tutor\\Hooks')) {
             new Tutor\Hooks();
         }
 
-        $video_service = null;
-        if (class_exists('MathCourse\\Video\\Video_Service')) {
-            $video_service = new Video\Video_Service();
-        }
+        $video_service = class_exists('MathCourse\\Video\\Video_Service')
+            ? new Video\Video_Service()
+            : null;
 
-        $token = null;
-        if (class_exists('MathCourse\\Video\\Token')) {
-            $token = new Video\Token();
-        }
+        $token = class_exists('MathCourse\\Video\\Token')
+            ? new Video\Token()
+            : null;
 
         if (class_exists('MathCourse\\Video\\Player')) {
             new Video\Player();
@@ -43,16 +40,19 @@ class Plugin
 
         if ($video_service && $access_service && $token) {
             if (class_exists('MathCourse\\Video\\Video_Endpoints')) {
-                $endpoints = new Video\Video_Endpoints($token, $video_service, $access_service);
-                $endpoints->register();
+                (new Video\Video_Endpoints($token, $video_service, $access_service))->register();
             }
 
-            if (class_exists('MathCourse\\Video\\Key')) {
-                (new Video\Key($token))->register();
+            $storage = class_exists('MathCourse\\Video\\HLS_Storage')
+                ? new Video\HLS_Storage()
+                : null;
+
+            if ($storage && class_exists('MathCourse\\Video\\Key')) {
+                (new Video\Key($token, $video_service, $storage))->register();
             }
 
-            if (class_exists('MathCourse\\Video\\HLS_Endpoints')) {
-                (new Video\HLS_Endpoints($token, $video_service))->register();
+            if ($storage && class_exists('MathCourse\\Video\\HLS_Endpoints')) {
+                (new Video\HLS_Endpoints($token, $video_service, $storage))->register();
             }
         }
     }
@@ -70,7 +70,7 @@ class Plugin
             wp_enqueue_script(
                 'mathcourse-player',
                 MATHCOURSE_URL . 'assets/js/player.js',
-                array('jquery'),
+                array(),
                 MATHCOURSE_VERSION,
                 true
             );
