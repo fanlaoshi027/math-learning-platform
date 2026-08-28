24f22caf9ee9dc28b4c6aeae1d458a1d2af91ac3
fanlaoshi027 <fanzeping@gmail.com>
2026-08-28 15:58:53 +0800

    Fix plugin module initialization against existing constructors

--- wp/wp-content/plugins/math-course/includes/class-plugin.php
+++ wp/wp-content/plugins/math-course/includes/class-plugin.php
@@ -14,24 +14,20 @@ class Plugin
 
     private function load_modules()
     {
-        // 后台菜单
         if (class_exists('MathCourse\\Admin\\Menu')) {
-            (new Admin\Menu())->register();
+            new Admin\Menu();
         }
 
-        // 授权服务
         if (class_exists('MathCourse\\Access\\Access_Service')) {
-            (new Access\Access_Service());
+            new Access\Access_Service();
         }
 
-        // Tutor LMS 对接
         if (class_exists('MathCourse\\Tutor\\Hooks')) {
-            (new Tutor\Hooks());
+            new Tutor\Hooks();
         }
 
-        // 视频播放器
         if (class_exists('MathCourse\\Video\\Player')) {
-            (new Video\Player());
+            new Video\Player();
         }
     }
 
