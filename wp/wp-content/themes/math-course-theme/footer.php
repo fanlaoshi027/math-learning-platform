f0e268c2b7479f3071ff512bd6ba11e9535662c3
fanlaoshi027 <fanzeping@gmail.com>
2026-08-29 21:01:24 +0800

    add standalone MathCourse theme footer

---
+++ wp/wp-content/themes/math-course-theme/footer.php
@@ -0,0 +1,12 @@
+<?php
+defined('ABSPATH') || exit;
+?>
+<footer class="mc-site-footer">
+    <div class="mc-container mc-site-footer__inner">
+        <span>© <?php echo esc_html(gmdate('Y')); ?> 樊老师数学</span>
+        <span>专注初中数学系统学习</span>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
