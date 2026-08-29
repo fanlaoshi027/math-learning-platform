d3fdd80f1d756be0c31eb7554014199c9e07ebc0
fanlaoshi027 <fanzeping@gmail.com>
2026-08-29 14:42:23 +0800

    Create math-course-theme front page template

---
+++ wp/wp-content/themes/math-course-theme/front-page.php
@@ -0,0 +1,19 @@
+<?php
+get_header();
+?>
+
+<main class="mc-container">
+
+<section class="mc-hero">
+    <h1>樊老师数学</h1>
+    <p>专注初中数学系统学习</p>
+</section>
+
+<section>
+    <?php echo do_shortcode('[mathcourse_course_center]'); ?>
+</section>
+
+</main>
+
+<?php
+get_footer();
