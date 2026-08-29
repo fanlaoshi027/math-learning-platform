54e85cbcd72f22016bef89fd0af41f2a8fb353e2
fanlaoshi027 <fanzeping@gmail.com>
2026-08-29 08:52:12 +0800

    feat: load course metadata module

--- wp/wp-content/plugins/math-course/includes/class-plugin.php
+++ wp/wp-content/plugins/math-course/includes/class-plugin.php
@@ -2,146 +2,61 @@
 
 namespace MathCourse;
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
+class Plugin {
 
-class Plugin
-{
+	public function run() {
+		$this->load_modules();
+		$this->load_assets();
+	}
 
+	private function load_modules() {
+		/* 后台 */
+		if ( class_exists( 'MathCourse\Admin\Menu' ) ) {
+			new Admin\Menu();
+		}
 
-    public function run()
-    {
+		/* 课程扩展字段 */
+		if ( class_exists( 'MathCourse\Course\Meta' ) ) {
+			new Course\Meta();
+		}
 
-        $this->load_modules();
+		/* 授权 */
+		if ( class_exists( 'MathCourse\Access\Access_Service' ) ) {
+			new Access\Access_Service();
+		}
 
-        $this->load_assets();
+		/* Tutor */
+		if ( class_exists( 'MathCourse\Tutor\Hooks' ) ) {
+			new Tutor\Hooks();
+		}
 
-    }
+		/* 视频 */
+		if ( class_exists( 'MathCourse\Video\Player' ) ) {
+			new Video\Player();
+		}
+	}
 
+	private function load_assets() {
+		add_action(
+			'wp_enqueue_scripts',
+			function() {
+				wp_enqueue_style(
+					'mathcourse',
+					MATHCOURSE_URL . 'assets/css/mathcourse.css',
+					array(),
+					MATHCOURSE_VERSION
+				);
 
-
-    private function load_modules()
-    {
-
-
-        /*
-        |--------------------------------------------------------------------------
-        | 后台
-        |--------------------------------------------------------------------------
-        */
-
-        if(
-            class_exists(
-                'MathCourse\Admin\Menu'
-            )
-        ){
-
-            new Admin\Menu();
-
-        }
-
-
-
-        /*
-        |--------------------------------------------------------------------------
-        | 授权
-        |--------------------------------------------------------------------------
-        */
-
-        if(
-            class_exists(
-                'MathCourse\Access\Access_Service'
-            )
-        ){
-
-            new Access\Access_Service();
-
-        }
-
-
-
-        /*
-        |--------------------------------------------------------------------------
-        | Tutor
-        |--------------------------------------------------------------------------
-        */
-
-        if(
-            class_exists(
-                'MathCourse\Tutor\Hooks'
-            )
-        ){
-
-            new Tutor\Hooks();
-
-        }
-
-
-
-        /*
-        |--------------------------------------------------------------------------
-        | 视频
-        |--------------------------------------------------------------------------
-        */
-
-        if(
-            class_exists(
-                'MathCourse\Video\Player'
-            )
-        ){
-
-            new Video\Player();
-
-        }
-
-
-    }
-
-
-
-    private function load_assets()
-    {
-
-
-        add_action(
-            'wp_enqueue_scripts',
-            function(){
-
-                wp_enqueue_style(
-
-                    'mathcourse',
-
-                    MATHCOURSE_URL .
-                    'assets/css/mathcourse.css',
-
-                    array(),
-
-                    MATHCOURSE_VERSION
-
-                );
-
-
-                wp_enqueue_script(
-
-                    'mathcourse-player',
-
-                    MATHCOURSE_URL .
-                    'assets/js/player.js',
-
-                    array('jquery'),
-
-                    MATHCOURSE_VERSION,
-
-                    true
-
-                );
-
-
-            }
-        );
-
-
-    }
-
-
+				wp_enqueue_script(
+					'mathcourse-player',
+					MATHCOURSE_URL . 'assets/js/player.js',
+					array( 'jquery' ),
+					MATHCOURSE_VERSION,
+					true
+				);
+			}
+		);
+	}
 }
