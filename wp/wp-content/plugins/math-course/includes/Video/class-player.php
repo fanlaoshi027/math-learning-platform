330bea3c3d18ab250765045727cbb0bc9e1fe923
fanlaoshi027 <fanzeping@gmail.com>
2026-08-29 14:17:53 +0800

    Improve MathCourse player shortcode for lesson progress support

--- wp/wp-content/plugins/math-course/includes/Video/class-player.php
+++ wp/wp-content/plugins/math-course/includes/Video/class-player.php
@@ -4,96 +4,56 @@ namespace MathCourse\Video;
 
 defined('ABSPATH') || exit;
 
-
 class Player
 {
 
-
     public function __construct()
     {
-
-
         add_shortcode(
-
             'mathcourse_video',
-
             array(
-
                 $this,
-
                 'render'
-
             )
-
         );
-
-
     }
 
 
-
-    public function render(
-        $atts
-    )
+    public function render($atts)
     {
-
-
-        $atts =
-        shortcode_atts(
-
+        $atts = shortcode_atts(
             array(
-
-                'url'=>''
-
+                'url' => '',
+                'lesson_id' => ''
             ),
-
             $atts
-
         );
 
-
-
-        if(
-            empty($atts['url'])
-        ){
-
+        if (empty($atts['url'])) {
             return '';
-
         }
 
-
+        $lesson_id = absint($atts['lesson_id']);
 
         ob_start();
-
-
         ?>
 
         <video
-
-        class="mathcourse-player"
-
-        controls
-
-        preload="metadata">
-
+            class="mathcourse-player"
+            <?php if ($lesson_id): ?>
+            data-lesson-id="<?php echo esc_attr($lesson_id); ?>"
+            <?php endif; ?>
+            controls
+            preload="metadata">
 
             <source
-
-            src="<?php echo esc_url($atts['url']); ?>"
-
-            type="application/x-mpegURL">
-
+                src="<?php echo esc_url($atts['url']); ?>"
+                type="application/x-mpegURL">
 
         </video>
 
-
         <?php
-
-
         return ob_get_clean();
-
-
     }
-
 
 }
