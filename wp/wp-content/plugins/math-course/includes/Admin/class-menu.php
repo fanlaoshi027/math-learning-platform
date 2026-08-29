17b050c705f288ec2537a61c80ed4382b588cb19
fanlaoshi027 <fanzeping@gmail.com>
2026-08-29 18:01:50 +0800

    完善教师后台菜单结构，增加学员管理和学习进度入口

--- wp/wp-content/plugins/math-course/includes/Admin/class-menu.php
+++ wp/wp-content/plugins/math-course/includes/Admin/class-menu.php
@@ -6,44 +6,97 @@ defined( 'ABSPATH' ) || exit;
 
 class Menu {
 
-	public function __construct() {
-		add_action( 'admin_menu', array( $this, 'register_menu' ) );
-	}
+    public function __construct() {
+        add_action( 'admin_menu', array( $this, 'register_menu' ) );
+    }
 
-	public function register_menu() {
-		add_menu_page( 'MathCourse', 'MathCourse', 'manage_options', 'mathcourse', array( $this, 'dashboard' ), 'dashicons-welcome-learn-more', 30 );
+    public function register_menu() {
 
-		add_submenu_page( 'mathcourse', '课程管理', '课程管理', 'manage_options', 'mathcourse-courses', array( $this, 'courses_page' ) );
-		add_submenu_page( 'mathcourse', '课程授权', '课程授权', 'manage_options', 'mathcourse-access', array( $this, 'access_page' ) );
-		add_submenu_page( 'mathcourse', '设置', '设置', 'manage_options', 'mathcourse-settings', array( $this, 'settings_page' ) );
-		add_submenu_page( null, '编辑课程', '编辑课程', 'manage_options', 'mathcourse-course-edit', array( $this, 'course_edit_page' ) );
-	}
+        add_menu_page(
+            'MathCourse',
+            '数学课程',
+            'manage_options',
+            'mathcourse',
+            array( $this, 'dashboard' ),
+            'dashicons-welcome-learn-more',
+            30
+        );
 
-	public function dashboard() {
-		echo '<div class="wrap"><h1>MathCourse</h1><p>数学课程管理系统</p></div>';
-	}
+        add_submenu_page(
+            'mathcourse',
+            '课程管理',
+            '课程管理',
+            'manage_options',
+            'mathcourse-courses',
+            array( $this, 'courses_page' )
+        );
 
-	public function courses_page() {
-		if ( class_exists( 'MathCourse\Admin\Course_Page' ) ) {
-			( new Course_Page() )->render();
-		}
-	}
+        add_submenu_page(
+            'mathcourse',
+            '课程授权',
+            '学员授权',
+            'manage_options',
+            'mathcourse-access',
+            array( $this, 'access_page' )
+        );
 
-	public function course_edit_page() {
-		if ( class_exists( 'MathCourse\Admin\Course_Editor' ) ) {
-			( new Course_Editor() )->render();
-		}
-	}
+        add_submenu_page(
+            'mathcourse',
+            '学习进度',
+            '学习进度',
+            'manage_options',
+            'mathcourse-progress',
+            array( $this, 'progress_page' )
+        );
 
-	public function access_page() {
-		if ( class_exists( 'MathCourse\Admin\Access_Page' ) ) {
-			( new Access_Page() )->render();
-		}
-	}
+        add_submenu_page(
+            'mathcourse',
+            '设置',
+            '系统设置',
+            'manage_options',
+            'mathcourse-settings',
+            array( $this, 'settings_page' )
+        );
 
-	public function settings_page() {
-		if ( class_exists( 'MathCourse\Admin\Settings' ) ) {
-			( new Settings() )->render();
-		}
-	}
+        add_submenu_page(
+            null,
+            '编辑课程',
+            '编辑课程',
+            'manage_options',
+            'mathcourse-course-edit',
+            array( $this, 'course_edit_page' )
+        );
+    }
+
+    public function dashboard() {
+        echo '<div class="wrap"><h1>MathCourse</h1><p>数学课程管理后台</p></div>';
+    }
+
+    public function courses_page() {
+        if ( class_exists( 'MathCourse\\Admin\\Course_Page' ) ) {
+            ( new Course_Page() )->render();
+        }
+    }
+
+    public function course_edit_page() {
+        if ( class_exists( 'MathCourse\\Admin\\Course_Editor' ) ) {
+            ( new Course_Editor() )->render();
+        }
+    }
+
+    public function access_page() {
+        if ( class_exists( 'MathCourse\\Admin\\Access_Page' ) ) {
+            ( new Access_Page() )->render();
+        }
+    }
+
+    public function progress_page() {
+        echo '<div class="wrap"><h1>学习进度</h1><p>查看学员课程完成情况</p></div>';
+    }
+
+    public function settings_page() {
+        if ( class_exists( 'MathCourse\\Admin\\Settings' ) ) {
+            ( new Settings() )->render();
+        }
+    }
 }
