9886964ba6f0f98e6d86cf898b77008c2d6b649f
fanlaoshi027 <fanzeping@gmail.com>
2026-08-30 22:49:34 +0800

    fix: initialize progress dependencies before course service

--- wp/wp-content/plugins/math-course/includes/class-plugin.php
+++ wp/wp-content/plugins/math-course/includes/class-plugin.php
@@ -6,6 +6,11 @@ class Plugin {
     public function run() { $this->load_modules(); $this->load_assets(); $this->load_admin_assets(); }
 
     private function load_modules() {
+        // Load foundational services before consumers that instantiate them.
+        if (class_exists('MathCourse\\Access\\Access_Service')) new Access\Access_Service();
+        if (class_exists('MathCourse\\Tutor\\Adapter')) new Tutor\Adapter();
+        if (class_exists('MathCourse\\Progress\\Progress_Service')) new Progress\Progress_Service();
+
         if (class_exists('MathCourse\\Admin\\Menu')) new Admin\Menu();
         if (class_exists('MathCourse\\Admin\\Course_Actions')) new Admin\Course_Actions();
         if (class_exists('MathCourse\\Admin\\Course_Sort')) new Admin\Course_Sort();
@@ -13,11 +18,8 @@ class Plugin {
         if (class_exists('MathCourse\\Course\\Meta')) new Course\Meta();
         if (class_exists('MathCourse\\Course\\Lesson_Meta')) new Course\Lesson_Meta();
         if (class_exists('MathCourse\\Course\\Course_Service')) new Course\Course_Service();
-        if (class_exists('MathCourse\\Access\\Access_Service')) new Access\Access_Service();
-        if (class_exists('MathCourse\\Progress\\Progress_Service')) new Progress\Progress_Service();
         if (class_exists('MathCourse\\Progress\\Progress_Ajax')) new Progress\Progress_Ajax();
         if (class_exists('MathCourse\\Tutor\\Hooks')) new Tutor\Hooks();
-        if (class_exists('MathCourse\\Tutor\\Adapter')) new Tutor\Adapter();
         if (class_exists('MathCourse\\Learning\\Lesson_Status')) new Learning\Lesson_Status();
         if (class_exists('MathCourse\\Learning\\Course_Learning')) new Learning\Course_Learning();
         if (class_exists('MathCourse\\Video\\Player')) new Video\Player();
