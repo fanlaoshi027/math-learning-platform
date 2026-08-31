073a58e7ecac75fa308a67f5cd61a4e4582bed9c
fanlaoshi027 <fanzeping@gmail.com>
2026-08-31 10:47:22 +0800

    wpui: enqueue course card UI polish

--- wp/wp-content/plugins/math-course/includes/Frontend/class-course-directory-assets.php
+++ wp/wp-content/plugins/math-course/includes/Frontend/class-course-directory-assets.php
@@ -13,6 +13,7 @@ class Course_Directory_Assets {
         if (!$post || !has_shortcode($post->post_content, 'mathcourse_course_directory') && !has_shortcode($post->post_content, 'mathcourse_course_center') && !has_shortcode($post->post_content, 'mathcourse_learning_center')) return;
 
         wp_enqueue_style('mathcourse-course-directory', MATHCOURSE_URL . 'assets/frontend-course-directory.css', array(), MATHCOURSE_VERSION);
+        wp_enqueue_style('mathcourse-course-directory-ui-polish', MATHCOURSE_URL . 'assets/course-directory-ui-polish.css', array('mathcourse-course-directory'), MATHCOURSE_VERSION);
         wp_enqueue_script('mathcourse-course-directory', MATHCOURSE_URL . 'assets/course-directory.js', array(), MATHCOURSE_VERSION, true);
     }
 }
