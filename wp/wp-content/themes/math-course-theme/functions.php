ba52877c3824692e12357e7dd3dde0d44656deee
fanlaoshi027 <fanzeping@gmail.com>
2026-08-31 11:16:26 +0800

    wpui: load header visual polish stylesheet

--- wp/wp-content/themes/math-course-theme/functions.php
+++ wp/wp-content/themes/math-course-theme/functions.php
@@ -32,6 +32,7 @@ function mc_theme_assets() {
 	$learning_polish_path=get_stylesheet_directory().'/assets/css/learning-ui-polish.css';
 	$home_path=get_stylesheet_directory().'/assets/css/home-ui-v2.css';
 	$reference_override_path=get_stylesheet_directory().'/assets/css/ui-reference-override.css';
+	$header_polish_path=get_stylesheet_directory().'/assets/css/header-ui-polish.css';
 	$version=file_exists($style_path)?(string)filemtime($style_path):'0.2.0';
 	$ui_version=file_exists($ui_path)?(string)filemtime($ui_path):'0.3.0';
 	$scale_version=file_exists($scale_path)?(string)filemtime($scale_path):'1.0.0';
@@ -43,6 +44,7 @@ function mc_theme_assets() {
 	$learning_polish_version=file_exists($learning_polish_path)?(string)filemtime($learning_polish_path):'1.0.0';
 	$home_version=file_exists($home_path)?(string)filemtime($home_path):'1.0.0';
 	$reference_override_version=file_exists($reference_override_path)?(string)filemtime($reference_override_path):'1.0.0';
+	$header_polish_version=file_exists($header_polish_path)?(string)filemtime($header_polish_path):'1.0.0';
 	wp_enqueue_style('mc-theme-style',get_stylesheet_uri(),array(),$version);
 	$queried_content=get_post_field('post_content',get_queried_object_id());
 	$load_course_ui=is_front_page()||is_page(array('course-center','xueyuan-denglu','learning'));
@@ -52,6 +54,7 @@ function mc_theme_assets() {
 		wp_enqueue_style('mc-ui-scale',get_stylesheet_directory_uri().'/assets/css/ui-scale.css',array('mc-reference-ui'),$scale_version);
 		wp_enqueue_style('mc-course-ui-polish',get_stylesheet_directory_uri().'/assets/css/course-ui-polish.css',array('mc-ui-scale'),$polish_version);
 	}
+	wp_enqueue_style('mc-header-ui-polish',get_stylesheet_directory_uri().'/assets/css/header-ui-polish.css',array('mc-theme-style'),$header_polish_version);
 	if(is_front_page()){
 		wp_enqueue_style('mc-home-ui-v2',get_stylesheet_directory_uri().'/assets/css/home-ui-v2.css',array('mc-course-ui-polish'),$home_version);
 	}
