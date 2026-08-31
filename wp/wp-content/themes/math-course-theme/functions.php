0a31ae8146dbc4dc60a33f709dd5fb2db65308c6
fanlaoshi027 <fanzeping@gmail.com>
2026-08-31 08:59:17 +0800

    wpui: load learning UI polish stylesheet

--- wp/wp-content/themes/math-course-theme/functions.php
+++ wp/wp-content/themes/math-course-theme/functions.php
@@ -29,6 +29,7 @@ function mc_theme_assets() {
 	$learning_states_path=get_stylesheet_directory().'/assets/css/learning-states.css';
 	$learning_nav_path=get_stylesheet_directory().'/assets/css/learning-nav.css';
 	$polish_path=get_stylesheet_directory().'/assets/css/course-ui-polish.css';
+	$learning_polish_path=get_stylesheet_directory().'/assets/css/learning-ui-polish.css';
 	$home_path=get_stylesheet_directory().'/assets/css/home-ui-v2.css';
 	$version=file_exists($style_path)?(string)filemtime($style_path):'0.2.0';
 	$ui_version=file_exists($ui_path)?(string)filemtime($ui_path):'0.3.0';
@@ -38,6 +39,7 @@ function mc_theme_assets() {
 	$states_version=file_exists($learning_states_path)?(string)filemtime($learning_states_path):'1.0.0';
 	$nav_version=file_exists($learning_nav_path)?(string)filemtime($learning_nav_path):'1.0.0';
 	$polish_version=file_exists($polish_path)?(string)filemtime($polish_path):'1.0.0';
+	$learning_polish_version=file_exists($learning_polish_path)?(string)filemtime($learning_polish_path):'1.0.0';
 	$home_version=file_exists($home_path)?(string)filemtime($home_path):'1.0.0';
 	wp_enqueue_style('mc-theme-style',get_stylesheet_uri(),array(),$version);
 	$queried_content=get_post_field('post_content',get_queried_object_id());
@@ -58,6 +60,7 @@ function mc_theme_assets() {
 		wp_enqueue_style('mc-learning',get_stylesheet_directory_uri().'/assets/css/learning.css',array('mc-ui-scale'),$learning_version);
 		wp_enqueue_style('mc-learning-states',get_stylesheet_directory_uri().'/assets/css/learning-states.css',array('mc-learning'),$states_version);
 		wp_enqueue_style('mc-learning-nav',get_stylesheet_directory_uri().'/assets/css/learning-nav.css',array('mc-learning-states'),$nav_version);
+		wp_enqueue_style('mc-learning-ui-polish',get_stylesheet_directory_uri().'/assets/css/learning-ui-polish.css',array('mc-learning-nav'),$learning_polish_version);
 	}
 }
 add_action('wp_enqueue_scripts','mc_theme_assets');
