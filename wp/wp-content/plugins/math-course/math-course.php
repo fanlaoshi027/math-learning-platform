fb54fd2720830c91c6202508d9780ab27515ce25
fanlaoshi027 <fanzeping@gmail.com>
2026-09-01 16:53:16 +0800

    chore: bump MathCourse asset version for player cache busting

--- wp/wp-content/plugins/math-course/math-course.php
+++ wp/wp-content/plugins/math-course/math-course.php
@@ -3,7 +3,7 @@
 Plugin Name: MathCourse
 Plugin URI:
 Description: 数学课程管理系统
-Version: 1.0.2
+Version: 1.0.3
 Author:
 Author URI:
 Text Domain: mathcourse
@@ -11,7 +11,7 @@ Text Domain: mathcourse
 
 defined('ABSPATH') || exit;
 
-define('MATHCOURSE_VERSION', '1.0.2');
+define('MATHCOURSE_VERSION', '1.0.3');
 define('MATHCOURSE_PATH', plugin_dir_path(__FILE__));
 define('MATHCOURSE_URL', plugin_dir_url(__FILE__));
 
@@ -32,32 +32,3 @@ register_activation_hook(__FILE__, function () {
     if (class_exists('\\MathCourse\\Access\\Access_Schema')) {
         \MathCourse\Access\Access_Schema::install();
     }
-
-    // Protected HLS 使用 WordPress rewrite route；激活时必须刷新一次规则。
-    if (class_exists('\\MathCourse\\Video\\Video_Router')) {
-        (new \MathCourse\Video\Video_Router())->register_route();
-        flush_rewrite_rules(false);
-    }
-});
-
-register_deactivation_hook(__FILE__, function () {
-    flush_rewrite_rules(false);
-});
-
-add_action('plugins_loaded', function () {
-
-    // 保证数据库结构存在
-    if (class_exists('\\MathCourse\\Access\\Access_Schema')) {
-        $version = get_option('mathcourse_access_db_version', '');
-
-        if ($version !== \MathCourse\Access\Access_Schema::VERSION) {
-            \MathCourse\Access\Access_Schema::install();
-        }
-    }
-
-    // 主插件启动
-    if (class_exists('\\MathCourse\\Plugin')) {
-        (new \MathCourse\Plugin())->run();
-    }
-
-});
