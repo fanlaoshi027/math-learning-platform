0faff8b469824a82fdaf97b1e4662e773d7b7759
fanlaoshi027 <fanzeping@gmail.com>
2026-09-01 20:51:23 +0800

    wpui-v2: restore MathCourse bootstrap and bump version

--- wp/wp-content/plugins/math-course/math-course.php
+++ wp/wp-content/plugins/math-course/math-course.php
@@ -32,3 +32,32 @@ register_activation_hook(__FILE__, function () {
     if (class_exists('\\MathCourse\\Access\\Access_Schema')) {
         \MathCourse\Access\Access_Schema::install();
     }
+
+    // Protected HLS 使用 WordPress rewrite route；激活时必须刷新一次规则。
+    if (class_exists('\\MathCourse\\Video\\Video_Router')) {
+        (new \MathCourse\Video\Video_Router())->register_route();
+        flush_rewrite_rules(false);
+    }
+});
+
+register_deactivation_hook(__FILE__, function () {
+    flush_rewrite_rules(false);
+});
+
+add_action('plugins_loaded', function () {
+
+    // 保证数据库结构存在
+    if (class_exists('\\MathCourse\\Access\\Access_Schema')) {
+        $version = get_option('mathcourse_access_db_version', '');
+
+        if ($version !== \MathCourse\Access\Access_Schema::VERSION) {
+            \MathCourse\Access\Access_Schema::install();
+        }
+    }
+
+    // 主插件启动
+    if (class_exists('\\MathCourse\\Plugin')) {
+        (new \MathCourse\Plugin())->run();
+    }
+
+});
