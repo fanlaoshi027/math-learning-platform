c34f53cdb1b8d3dd2b61935c1b4894a00a2c92b7
fanlaoshi027 <fanzeping@gmail.com>
2026-09-04 00:06:05 +0800

    chore: bump MathCourse version

--- wp/wp-content/plugins/math-course/math-course.php
+++ wp/wp-content/plugins/math-course/math-course.php
@@ -3,7 +3,7 @@
 Plugin Name: MathCourse
 Plugin URI:
 Description: 数学课程管理系统
-Version: 1.0.6
+Version: 1.0.7
 Author:
 Author URI:
 Text Domain: mathcourse
@@ -11,7 +11,7 @@ Text Domain: mathcourse
 
 defined('ABSPATH') || exit;
 
-define('MATHCOURSE_VERSION', '1.0.6');
+define('MATHCOURSE_VERSION', '1.0.7');
 define('MATHCOURSE_PATH', plugin_dir_path(__FILE__));
 define('MATHCOURSE_URL', plugin_dir_url(__FILE__));
 
