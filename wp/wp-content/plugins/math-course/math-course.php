92f205f6fe8505e015e0b4e8a2cec00aee964e21
fanlaoshi027 <fanzeping@gmail.com>
2026-09-03 16:10:29 +0800

    确保 HLS 网关路由在版本更新后自动刷新

--- wp/wp-content/plugins/math-course/math-course.php
+++ wp/wp-content/plugins/math-course/math-course.php
@@ -3,7 +3,7 @@
 Plugin Name: MathCourse
 Plugin URI:
 Description: 数学课程管理系统
-Version: 1.0.3
+Version: 1.0.4
 Author:
 Author URI:
 Text Domain: mathcourse
@@ -11,12 +11,12 @@ Text Domain: mathcourse
 
 defined('ABSPATH') || exit;
 
-define('MATHCOURSE_VERSION', '1.0.3');
+define('MATHCOURSE_VERSION', '1.0.4');
 define('MATHCOURSE_PATH', plugin_dir_path(__FILE__));
 define('MATHCOURSE_URL', plugin_dir_url(__FILE__));
 
 require_once MATHCOURSE_PATH . 'includes/class-autoloader.php';
-\MathCourse\Autoloader::register();
+\\MathCourse\\Autoloader::register();
 
 /**
  * 插件激活
@@ -26,18 +26,20 @@ require_once MATHCOURSE_PATH . 'includes/class-autoloader.php';
  */
 register_activation_hook(__FILE__, function () {
     if (class_exists('\\MathCourse\\Database\\Install')) {
-        \MathCourse\Database\Install::activate();
+        \\MathCourse\\Database\\Install::activate();
     }
 
-    if (class_exists('\\MathCourse\\Access\Access_Schema')) {
-        \MathCourse\Access\Access_Schema::install();
+    if (class_exists('\\MathCourse\\Access\\Access_Schema')) {
+        \\MathCourse\\Access\\Access_Schema::install();
     }
 
     // Protected HLS 使用 WordPress rewrite route；激活时必须刷新一次规则。
-    if (class_exists('\\MathCourse\Video\Video_Router')) {
-        (new \MathCourse\Video\Video_Router())->register_route();
+    if (class_exists('\\MathCourse\\Video\\Video_Router')) {
+        (new \\MathCourse\\Video\\Video_Router())->register_route();
         flush_rewrite_rules(false);
     }
+
+    update_option('mathcourse_rewrite_version', MATHCOURSE_VERSION, false);
 });
 
 register_deactivation_hook(__FILE__, function () {
@@ -47,17 +49,26 @@ register_deactivation_hook(__FILE__, function () {
 add_action('plugins_loaded', function () {
 
     // 保证数据库结构存在
-    if (class_exists('\\MathCourse\Access\Access_Schema')) {
+    if (class_exists('\\MathCourse\\Access\\Access_Schema')) {
         $version = get_option('mathcourse_access_db_version', '');
 
-        if ($version !== \MathCourse\Access\Access_Schema::VERSION) {
-            \MathCourse\Access\Access_Schema::install();
+        if ($version !== \\MathCourse\\Access\\Access_Schema::VERSION) {
+            \\MathCourse\\Access\\Access_Schema::install();
+        }
+    }
+
+    // 版本更新后刷新一次 Protected HLS rewrite rules，避免旧站点继续返回 404。
+    if (get_option('mathcourse_rewrite_version', '') !== MATHCOURSE_VERSION) {
+        if (class_exists('\\MathCourse\\Video\\Video_Router')) {
+            (new \\MathCourse\\Video\\Video_Router())->register_route();
+            flush_rewrite_rules(false);
+            update_option('mathcourse_rewrite_version', MATHCOURSE_VERSION, false);
         }
     }
 
     // 主插件启动
-    if (class_exists('\\MathCourse\Plugin')) {
-        (new \MathCourse\Plugin())->run();
+    if (class_exists('\\MathCourse\\Plugin')) {
+        (new \\MathCourse\\Plugin())->run();
     }
 
 });
