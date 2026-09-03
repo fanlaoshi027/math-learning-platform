933a6a91e66566674ed2a781cf1824e420a8a019
fanlaoshi027 <fanzeping@gmail.com>
2026-09-04 01:12:07 +0800

    feat: load activation modules

--- wp/wp-content/plugins/math-course/includes/class-plugin.php
+++ wp/wp-content/plugins/math-course/includes/class-plugin.php
@@ -4,12 +4,14 @@ defined('ABSPATH') || exit;
 
 class Plugin {
     public function run() { $this->load_modules(); $this->load_assets(); $this->load_admin_assets(); }
-
     private function load_modules() {
         if (class_exists('MathCourse\\Access\\Access_Service')) new Access\Access_Service();
+        if (class_exists('MathCourse\\Access\\Activation_Schema')) new Access\Activation_Schema();
+        if (class_exists('MathCourse\\Access\\Activation_Service')) new Access\Activation_Service();
         if (class_exists('MathCourse\\Tutor\\Adapter')) new Tutor\Adapter();
         if (class_exists('MathCourse\\Progress\\Progress_Service')) new Progress\Progress_Service();
         if (class_exists('MathCourse\\Admin\\Menu')) new Admin\Menu();
+        if (class_exists('MathCourse\\Admin\\Activation_Page')) new Admin\Activation_Page();
         if (class_exists('MathCourse\\Admin\\Course_Actions')) new Admin\Course_Actions();
         if (class_exists('MathCourse\\Admin\\Course_Sort')) new Admin\Course_Sort();
         if (class_exists('MathCourse\\Admin\\Batch_Manager')) new Admin\Batch_Manager();
@@ -23,22 +25,11 @@ class Plugin {
         if (class_exists('MathCourse\\Video\\Player')) new Video\Player();
         if (class_exists('MathCourse\\Video\\Video_Router')) new Video\Video_Router();
         if (class_exists('MathCourse\\Video\\Hls_Accelerator')) new Video\Hls_Accelerator();
+        if (class_exists('MathCourse\\Frontend\\Activation')) new Frontend\Activation();
         if (class_exists('MathCourse\\Frontend\\Course_Directory')) new Frontend\Course_Directory();
         if (class_exists('MathCourse\\Frontend\\Course_Player')) new Frontend\Course_Player();
         if (class_exists('MathCourse\\Frontend\\Course_Directory_Assets')) new Frontend\Course_Directory_Assets();
     }
-
-    private function load_assets() {
-        add_action('wp_enqueue_scripts', function() {
-            wp_enqueue_style('mathcourse', MATHCOURSE_URL.'assets/css/mathcourse.css', array(), MATHCOURSE_VERSION);
-        });
-    }
-
-    private function load_admin_assets() {
-        add_action('admin_enqueue_scripts', function() {
-            wp_enqueue_style('mathcourse-admin', MATHCOURSE_URL.'assets/css/admin.css', array(), MATHCOURSE_VERSION);
-            wp_enqueue_style('mathcourse-admin-redesign', MATHCOURSE_URL.'assets/css/admin-redesign.css', array('mathcourse-admin'), MATHCOURSE_VERSION);
-            wp_enqueue_script('mathcourse-admin-course-editor', MATHCOURSE_URL.'assets/js/admin-course-editor.js', array(), MATHCOURSE_VERSION, true);
-        });
-    }
+    private function load_assets() { add_action('wp_enqueue_scripts', function() { wp_enqueue_style('mathcourse', MATHCOURSE_URL.'assets/css/mathcourse.css', array(), MATHCOURSE_VERSION); }); }
+    private function load_admin_assets() { add_action('admin_enqueue_scripts', function() { wp_enqueue_style('mathcourse-admin', MATHCOURSE_URL.'assets/css/admin.css', array(), MATHCOURSE_VERSION); wp_enqueue_style('mathcourse-admin-redesign', MATHCOURSE_URL.'assets/css/admin-redesign.css', array('mathcourse-admin'), MATHCOURSE_VERSION); wp_enqueue_script('mathcourse-admin-course-editor', MATHCOURSE_URL.'assets/js/admin-course-editor.js', array(), MATHCOURSE_VERSION, true); }); }
 }
