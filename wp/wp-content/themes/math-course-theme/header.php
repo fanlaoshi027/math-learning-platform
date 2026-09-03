78368b0f0e298423ada1cb657c2fc4c3ad0dae38
fanlaoshi027 <fanzeping@gmail.com>
2026-09-03 15:29:09 +0800

    重做前台统一导航与品牌头部

--- wp/wp-content/themes/math-course-theme/header.php
+++ wp/wp-content/themes/math-course-theme/header.php
@@ -12,7 +12,8 @@ defined( 'ABSPATH' ) || exit;
 <header class="mc-site-header">
     <div class="mc-container mc-site-header__inner">
         <a class="mc-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" aria-label="樊老师数学课堂首页">
-            <span class="mc-brand__text">樊老师<span>数学课堂</span></span>
+            <span class="mc-brand__mark" aria-hidden="true">□</span>
+            <span class="mc-brand__text">樊老师数学课堂<small>跟中考名师 学扎实数学</small></span>
         </a>
 
         <nav class="mc-site-nav" aria-label="主导航">
@@ -21,23 +22,19 @@ defined( 'ABSPATH' ) || exit;
             <?php if ( is_user_logged_in() ) : ?>
                 <a class="<?php echo is_page( 'learning-center' ) ? 'is-active' : ''; ?>" href="<?php echo esc_url( home_url( '/learning-center/' ) ); ?>">学习中心</a>
             <?php endif; ?>
-            <a class="<?php echo is_page( 'about-teacher' ) ? 'is-active' : ''; ?>" href="<?php echo esc_url( home_url( '/about-teacher/' ) ); ?>">关于老师</a>
         </nav>
 
         <form class="mc-header-search" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-            <input type="search" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" placeholder="搜索数学知识点" aria-label="搜索数学知识点">
-            <button type="submit">搜索</button>
+            <input type="search" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" placeholder="搜索课程或专题" aria-label="搜索课程或专题">
+            <button type="submit" aria-label="搜索">⌕</button>
         </form>
 
         <div class="mc-header-actions">
             <?php if ( is_user_logged_in() ) : ?>
-                <a class="mc-user-pill" href="<?php echo esc_url( home_url( '/learning-center/' ) ); ?>">我的学习 <span class="mc-user-pill__arrow" aria-hidden="true">→</span></a>
+                <a class="mc-user-pill" href="<?php echo esc_url( home_url( '/learning-center/' ) ); ?>"><b>樊</b><span>樊同学</span><i>|</i><span>退出</span></a>
             <?php else : ?>
                 <?php $login_url = wp_login_url( home_url( '/learning-center/' ) ); ?>
-                <a class="mc-header-login" href="<?php echo esc_url( $login_url ); ?>">登录</a>
-                <?php if ( get_option( 'users_can_register' ) ) : ?>
-                    <a class="mc-header-register" href="<?php echo esc_url( wp_registration_url() ); ?>">注册</a>
-                <?php endif; ?>
+                <a class="mc-header-login" href="<?php echo esc_url( $login_url ); ?>">学生登录</a>
             <?php endif; ?>
         </div>
     </div>
