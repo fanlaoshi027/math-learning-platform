4a4d024864474998ec7b4b8fb0365e0789c8bcaf
fanlaoshi027 <fanzeping@gmail.com>
2026-09-03 16:10:38 +0800

    修正插件启动文件并保留路由自动刷新

--- wp/wp-content/plugins/math-course/math-course.php
+++ wp/wp-content/plugins/math-course/math-course.php
@@ -16,7 +16,7 @@ define('MATHCOURSE_PATH', plugin_dir_path(__FILE__));
 define('MATHCOURSE_URL', plugin_dir_url(__FILE__));
 
 require_once MATHCOURSE_PATH . 'includes/class-autoloader.php';
-\\MathCourse\\Autoloader::register();
+\MathCourse\Autoloader::register();
 
 /**
  * 插件激活
@@ -26,16 +26,16 @@ require_once MATHCOURSE_PATH . 'includes/class-autoloader.php';
  */
 register_activation_hook(__FILE__, function () {
     if (class_exists('\\MathCourse\\Database\\Install')) {
-        \\MathCourse\\Database\\Install::activate();
+        \MathCourse\Database\Install::activate();
     }
 
     if (class_exists('\\MathCourse\\Access\\Access_Schema')) {
-        \\MathCourse\\Access\\Access_Schema::install();
+        \MathCourse\Access\Access_Schema::install();
     }
 
     // Protected HLS 使用 WordPress rewrite route；激活时必须刷新一次规则。
     if (class_exists('\\MathCourse\\Video\\Video_Router')) {
-        (new \\MathCourse\\Video\\Video_Router())->register_route();
+        (new \MathCourse\Video\Video_Router())->register_route();
         flush_rewrite_rules(false);
     }
 
@@ -52,15 +52,15 @@ add_action('plugins_loaded', function () {
     if (class_exists('\\MathCourse\\Access\\Access_Schema')) {
         $version = get_option('mathcourse_access_db_version', '');
 
-        if ($version !== \\MathCourse\\Access\\Access_Schema::VERSION) {
-            \\MathCourse\\Access\\Access_Schema::install();
+        if ($version !== \MathCourse\Access\Access_Schema::VERSION) {
+            \MathCourse\Access\Access_Schema::install();
         }
     }
 
     // 版本更新后刷新一次 Protected HLS rewrite rules，避免旧站点继续返回 404。
     if (get_option('mathcourse_rewrite_version', '') !== MATHCOURSE_VERSION) {
         if (class_exists('\\MathCourse\\Video\\Video_Router')) {
-            (new \\MathCourse\\Video\\Video_Router())->register_route();
+            (new \MathCourse\Video\Video_Router())->register_route();
             flush_rewrite_rules(false);
             update_option('mathcourse_rewrite_version', MATHCOURSE_VERSION, false);
         }
@@ -68,7 +68,7 @@ add_action('plugins_loaded', function () {
 
     // 主插件启动
     if (class_exists('\\MathCourse\\Plugin')) {
-        (new \\MathCourse\\Plugin())->run();
+        (new \MathCourse\Plugin())->run();
     }
 
 });
