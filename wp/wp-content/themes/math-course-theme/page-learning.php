79648957bdf4e9e81fd6831755978e77acf59813
fanlaoshi027 <fanzeping@gmail.com>
2026-09-03 17:06:47 +0800

    移除学习页多余容器，让课程播放器直接占据学习页

--- wp/wp-content/themes/math-course-theme/page-learning.php
+++ wp/wp-content/themes/math-course-theme/page-learning.php
@@ -14,24 +14,22 @@ $lesson_id = isset( $_GET['lesson_id'] ) ? absint( $_GET['lesson_id'] ) : 0;
 ?>
 
 <main class="mc-learning-page">
-	<div class="mc-learning-container">
-		<?php
-		if ( $course_id && shortcode_exists( 'mathcourse_course_player' ) ) {
-			echo do_shortcode(
-				'[mathcourse_course_player course_id="' . esc_attr( $course_id ) . '"' .
-				( $lesson_id ? ' lesson_id="' . esc_attr( $lesson_id ) . '"' : '' ) .
-				']'
-			);
-		} else {
-			?>
-			<section class="mc-lesson-card">
-				<h1>课程不存在或链接无效</h1>
-				<p><a href="<?php echo esc_url( home_url( '/course-center/' ) ); ?>">返回课程中心</a></p>
-			</section>
-			<?php
-		}
+	<?php
+	if ( $course_id && shortcode_exists( 'mathcourse_course_player' ) ) {
+		echo do_shortcode(
+			'[mathcourse_course_player course_id="' . esc_attr( $course_id ) . '"' .
+			( $lesson_id ? ' lesson_id="' . esc_attr( $lesson_id ) . '"' : '' ) .
+			']'
+		);
+	} else {
 		?>
-	</div>
+		<section class="mc-lesson-card">
+			<h1>课程不存在或链接无效</h1>
+			<p><a href="<?php echo esc_url( home_url( '/course-center/' ) ); ?>">返回课程中心</a></p>
+		</section>
+		<?php
+	}
+	?>
 </main>
 
 <?php get_footer();
