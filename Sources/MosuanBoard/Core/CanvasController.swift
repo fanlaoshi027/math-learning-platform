b0f97b3aa5b8cdb410f0f91fe71ac82b9fd109c3
fanlaoshi027 <fanzeping@gmail.com>
2026-09-08 07:03:20 +0800

    Expose selection state to toolbar

--- Sources/MosuanBoard/Core/CanvasController.swift
+++ Sources/MosuanBoard/Core/CanvasController.swift
@@ -4,6 +4,7 @@ final class CanvasController: ObservableObject {
     weak var canvas: InkMetalView?
     @Published private(set) var canUndo = false
     @Published private(set) var canRedo = false
+    @Published private(set) var hasSelection = false
 
     func attach(_ canvas: InkMetalView) {
         self.canvas = canvas
@@ -11,6 +12,10 @@ final class CanvasController: ObservableObject {
             guard let self, let canvas else { return }
             self.canUndo = canvas.canUndo
             self.canRedo = canvas.canRedo
+        }
+        canvas.onSelectionChanged = { [weak self, weak canvas] in
+            guard let self, let canvas else { return }
+            self.hasSelection = canvas.hasSelection
         }
         refreshState()
     }
@@ -25,8 +30,14 @@ final class CanvasController: ObservableObject {
         refreshState()
     }
 
+    func deleteSelected() {
+        canvas?.deleteSelected()
+        refreshState()
+    }
+
     func refreshState() {
         canUndo = canvas?.canUndo ?? false
         canRedo = canvas?.canRedo ?? false
+        hasSelection = canvas?.hasSelection ?? false
     }
 }
