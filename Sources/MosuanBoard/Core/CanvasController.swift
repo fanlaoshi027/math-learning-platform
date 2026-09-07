2dd69833eaf8d93c32c1fcb1504c2bf8d4c58f3d
fanlaoshi027 <fanzeping@gmail.com>
2026-09-08 06:40:40 +0800

    Add canvas undo redo controller

---
+++ Sources/MosuanBoard/Core/CanvasController.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+final class CanvasController: ObservableObject {
+    weak var canvas: InkMetalView?
+
+    @Published private(set) var canUndo = false
+    @Published private(set) var canRedo = false
+
+    func attach(_ canvas: InkMetalView) {
+        self.canvas = canvas
+        refreshState()
+    }
+
+    func undo() {
+        canvas?.undo()
+        refreshState()
+    }
+
+    func redo() {
+        canvas?.redo()
+        refreshState()
+    }
+
+    func refreshState() {
+        canUndo = canvas?.canUndo ?? false
+        canRedo = canvas?.canRedo ?? false
+    }
+}
