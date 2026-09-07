1f6d7e7f6c77fe65199c37f12528c2fc2c2ecd09
fanlaoshi027 <fanzeping@gmail.com>
2026-09-08 00:38:59 +0800

    feat: add Mosuan Board Swift package skeleton

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 5.10
+import PackageDescription
+
+let package = Package(
+    name: "MosuanBoard",
+    platforms: [
+        .macOS(.v14)
+    ],
+    products: [
+        .executable(name: "MosuanBoard", targets: ["MosuanBoard"])
+    ],
+    targets: [
+        .executableTarget(
+            name: "MosuanBoard",
+            path: "Sources/MosuanBoard"
+        )
+    ]
+)
