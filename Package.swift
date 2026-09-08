db4ff3afe1aad597d78702d2bbbe0e8853dc1f1a
fanlaoshi027 <fanzeping@gmail.com>
2026-09-08 16:39:01 +0800

    exclude superseded renderer and board screen implementations

--- Package.swift
+++ Package.swift
@@ -3,9 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "MosuanBoard",
-    platforms: [
-        .macOS(.v14)
-    ],
+    platforms: [.macOS(.v14)],
     products: [
         .executable(name: "MosuanBoard", targets: ["MosuanBoard"])
     ],
@@ -13,6 +11,11 @@ let package = Package(
         .executableTarget(
             name: "MosuanBoard",
             path: "Sources/MosuanBoard",
+            exclude: [
+                "BoardScreen.swift",
+                "Metal/InkRenderer.swift",
+                "Core/GraphicObjectStore.swift"
+            ],
             resources: [
                 .process("Metal/InkShaders.metal")
             ]
