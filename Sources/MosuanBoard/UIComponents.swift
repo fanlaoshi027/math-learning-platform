d773fa32cf2216b21df918f7fb1f11f40de8a18e
fanlaoshi027 <fanzeping@gmail.com>
2026-09-09 13:11:10 +0800

    feat: add dynamic angle board tool

--- Sources/MosuanBoard/UIComponents.swift
+++ Sources/MosuanBoard/UIComponents.swift
@@ -2,7 +2,7 @@ import SwiftUI
 import AppKit
 
 /// Shared board UI types used by the current macOS screen.
-enum BoardTool: Equatable { case select, pen, line, smartLine, polygon, eraser }
+enum BoardTool: Equatable { case select, pen, line, smartLine, polygon, dynamicAngle, eraser }
 
 enum BoardBackground: String, CaseIterable, Identifiable {
     case white, black, darkGray, lightGray, cream
@@ -133,11 +133,12 @@ struct MetalInkCanvas: NSViewRepresentable {
     }
 
     private func configure(_ view: InkMetalView) {
-        view.isUserInteractionEnabledForTool = tool == .pen || tool == .line || tool == .smartLine || tool == .polygon
+        view.isUserInteractionEnabledForTool = tool == .pen || tool == .line || tool == .smartLine || tool == .polygon || tool == .dynamicAngle
         view.isSelectionTool = tool == .select
         view.isLineTool = tool == .line
         view.isSmartLineTool = tool == .smartLine
         view.isPolygonTool = tool == .polygon
+        view.isDynamicAngleTool = tool == .dynamicAngle
         view.isEraserTool = tool == .eraser
         view.penStyle = penStyle
         view.boardBackground = background
