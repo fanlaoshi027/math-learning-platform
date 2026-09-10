c46ea0340d5954fb64cffa8372e66f4ee7dc782b
fanlaoshi027 <fanzeping@gmail.com>
2026-09-10 23:25:03 +0800

    feat: add import PDF menu command

--- Sources/MosuanBoard/MosuanBoardApp.swift
+++ Sources/MosuanBoard/MosuanBoardApp.swift
@@ -12,5 +12,13 @@ struct MosuanBoardApp: App {
         }
         .windowStyle(.titleBar)
         .windowToolbarStyle(.unified)
+        .commands {
+            CommandGroup(after: .newItem) {
+                Button("导入 PDF…") {
+                    PDFWorkspaceController.shared.openPDF()
+                }
+                .keyboardShortcut("o", modifiers: [.command, .shift])
+            }
+        }
     }
 }
